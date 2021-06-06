ff45d4ca4bd779cfe69d187ab7e2eeb27d51835e
kevinccbsg <kevinccbsg@gmail.com>
2021-06-07 00:55:01 +0200

    feat: add demo examples

--- UI-e2e/cypress/integration/demo.spec.js
+++ UI-e2e/cypress/integration/demo.spec.js
@@ -5,15 +5,50 @@ describe('DCSL Talk demo examples', () => {
     cy.visit('/');
   });
 
-  // it('Selectors', () => {});
+  describe('Cypress concepts examples', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
 
-  // it('Assertions', () => {});
+    it('Selectors', () => {
+      cy.get('.login input');
+      cy.get('.login input').eq(0);
+      // cy.get('[data-cy=username]');
+    });
 
-  // it('Key events', () => {});
+    it('Key events', () => {
+      const typedText = 'new user';
+      cy.get('[data-cy=username]').type(typedText);
+      cy.get('[data-cy=password]').type(typedText);
+    });
 
-  // it('Use of fixture variables', () => {});
+    it('Assertions', () => {
+      const typedText = 'new user';
+      cy.get('[data-cy=username]').type(typedText).should('have.value', typedText);
+      cy.get('[data-cy=password]').type(typedText).should('have.value', typedText);
+    });
 
-  // it('Server mocking', () => {});
 
-  // it('Videos, commands & screenshots', () => {});
+    it('Server mocking', () => {
+      // cy.intercept('POST', '/api/v1/login')
+      //   .as('loginRequest');
+      // cy.intercept('GET', '/api/v1/tickets')
+      //   .as('getTicketsRequest');
+      // Mock response
+      cy.intercept('POST', '/api/v1/login', { fixture: 'login.json' })
+        .as('loginRequest');
+      cy.intercept('GET', '/api/v1/tickets', { fixture: 'tickets.json' })
+        .as('getTicketsRequest');
+      cy.get('[data-cy=username]').type('test');
+      cy.get('[data-cy=password]').type('test1234');
+      cy.get('[data-cy=btn]').click();
+      cy.wait(['@loginRequest', '@getTicketsRequest'], { timeout: 10000 });
+      cy.url().should('not.contain', '/login');
+    });
+
+    it('commands & screenshots', () => {
+      cy.typeLogin('test', 'test1234');
+      cy.screenshot('[data-cy=username]');
+    });
+  });
 });
